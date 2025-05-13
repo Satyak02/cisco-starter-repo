6e6095bf0daec57aaf6aa0787ab976c38cad0897
Satya Kadali <satyapraneethk.02@gmail.com>
2025-05-13 17:58:22 -0400

    Add Banner and Exhibit Components

---
+++ sextant/src/components/banner.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './banner.css';
+
+function Banner() {
+  return (
+    <div className="banner">
+      <h1>Sextant Dashboard</h1>
+    </div>
+  );
+}
+
+export default Banner;
+
